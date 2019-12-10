2bc27d2c101d9eb6d232179fd66c7c8442a11601
Shane Howearth <Shane.Howearth@shanehowearth.com>
2019-12-10 17:02:09 +1100

    Database

---
+++ accessdb/init.sql
@@ -0,0 +1,11 @@
+CREATE USER accesswriter;
+CREATE DATABASE access_postgres_db owner accesswriter;
+GRANT ALL PRIVILEGES ON DATABASE access_postgres_db TO accesswriter;
+\connect access_postgres_db accesswriter
+CREATE TABLE IF NOT EXISTS access
+(
+	id SERIAL PRIMARY KEY,
+	ip VARCHAR(45),
+	access_time time(6)
+);
+ALTER USER accesswriter WITH PASSWORD 'hu8jmn3';
